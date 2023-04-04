7a7d9576039f3672b0e297493bfe77199a26cb29
Elvin Bajric <elvin.bajricc@gmail.com>
2023-04-04 19:25:44 +0200

    Added more stuff from astrocommunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,16 @@ return {
 
   { import = "astrocommunity.colorscheme.nightfox" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
